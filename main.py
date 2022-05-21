f2afa5e9edc4c0c1a08678d4e12e4edb78dcbcb6
lUCAS tadeu <101116879+Lucaass6072@users.noreply.github.com>
2022-05-21 00:42:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import discord
+from discord import app_commands
+import random
+
+id_do_servidor = 956586784651878400 #Coloque aqui o ID do seu servidor
+
+class client(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.synced = False #Nós usamos isso para o bot não sincronizar os comandos mais de uma vez
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced: #Checar se os comandos slash foram sincronizados 
+            await tree.sync(guild = discord.Object(id=id_do_servidor)) # Você também pode deixar o id do servidor em branco para aplicar em todos servidores, mas isso fará com que demore de 1~24 horas para funcionar.
+            self.synced = True
+        print(f"Entramos como {self.user}.")
+
+aclient = client()
+tree = app_commands.CommandTree(aclient)
+
+@tree.command(guild = discord.Object(id=id_do_servidor), name = 'teste', description='Testando') #Comando específico para seu servidor
+async def slash2(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Estou funcionando!", ephemeral = True) 
+
+@tree.command(guild = discord.Object(id=id_do_servidor), name = 'ola', description='wadad') #Comando específico para seu servidor
+async def slash4(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Estou funcionando!", ephemeral = True)
+
+
+
+
+
+
+  #evsntos
+
+
+
+aclient.run('OTUwNDIxNTEzMjIyMzY1MjE0.YiYrAg.8KzcgJMiwOAWX2Bsgf9t0Y5bwv0')
